e71792f51d1c5877e2769a81d86669d5454c94ba
probloggerrohit <probloggerrohit@gmail.com>
2024-11-25 04:31:21 +0530

    Logging

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -707,7 +707,7 @@ const settingBank = async (req, res) => {
             ])
 
             return res.status(200).json({
-                message: 'Successfully changed',
+                message: 'Successfully changed sssss',
                 status: true,
                 datas: recharge,
             });
